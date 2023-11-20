a576989efc1b6564a055ba4e031bf7a9773063ba
Russel Otieno <russel.docs@gmail.com>
2023-11-19 21:20:50 -0800

    added intepreter path

---
+++ most_active_cookie.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from CommandLineParser import CommandLineParser
+
+# Driver Code to Run actual program on terminal
+
+
+def get_most_frequent_cookie():
+    cli_parser = CommandLineParser()
+    cli_parser.run()
+
+
+
+if __name__ == "__main__":
+   get_most_frequent_cookie()
+    
+
